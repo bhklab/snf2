d9d7aa251c7e0b0cf078d96e0d0981a9684fbde6
Michael Tran <mtran256@uwo.ca>
2026-09-03 14:16:42 -0400

    fix: sort imports and update __all__ declaration

--- src/snf2/__init__.py
+++ src/snf2/__init__.py
@@ -1,6 +1,6 @@
 """SNF2: a modern Python implementation of Similarity Network Fusion."""
 
-from snf2.affinity import make_affinity,affinity_matrix
+from snf2.affinity import affinity_matrix, make_affinity
 from snf2.fusion import fuse
 
-__all__ = ["fuse", "make_affinity","affinity_matrix"]
+__all__ = ["affinity_matrix", "fuse", "make_affinity"]
